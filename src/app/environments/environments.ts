53fec2e36e2c25c4d1c39624a2729cebfbcbb99e
poomsak <poomsak1994@gmail.com>
2025-07-07 20:23:40 +0700

    update ล่าาสุด

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,7 +1,6 @@
 export const environment = {
     config: {
         baseConfig: {
-            // apiUrl: "https://e-govs.com",
             apiUrl: "https://qm-iot.online",
             // apiUrl: "http://152.42.195.61:5000"
             // apiUrl: "http://127.0.0.1:5000"
